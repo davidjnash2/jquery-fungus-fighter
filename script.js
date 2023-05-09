14d61992436cad4437bb0e3bc1f85e6d6e3e8c83
Dave Nash <davidjnash2@gmail.com>
2023-05-09 00:25:28 -0500

    cleaned up script and commented lines

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
 $(document).ready(onReady);
 
-// State Variables can be declared outside of the onReady
-// Feel free to make this to what you want!
-// Example:
-// let fungusHP = 100;
-
-
 let fungusHP = 100; // declare global variable to set starting value for HP
 let myAP = 100; // declare global variable to set starting value for AP
 
 
 
-function onReady() {
+function onReady() { // declare onReady function
     console.log('jQuery ready!');
-    $('#scepterButt').on('click', scepterAttack);
-    $('#entangleButt').on('click', entangleAttack);
-    $('#bladeButt').on('click', bladeAttack);
-    $('#fireButt').on('click', fireAttack);
-
-
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
+    $('#scepterButt').on('click', scepterAttack); // event listener to register scepter attack button click
+    $('#entangleButt').on('click', entangleAttack); // event listener to register entangle attack button click
+    $('#bladeButt').on('click', bladeAttack); // event listener to register blade attack button click
+    $('#fireButt').on('click', fireAttack); // event listener to register fire attack button click
 }
    
-function render() {
+function render() { // declare render function
 
-    if ( myAP < 0 ) {
+    if ( myAP < 0 ) { // conditional to set my score to zero if it goes negative
         myAP = 0
-    };
+    }; // end conditional
     
-    if ( fungusHP < 0 ) {
+    if ( fungusHP < 0 ) { // conditional to set fungus score to zero if it goes negative
         fungusHP = 0
-    };
+    }; // end conditional
     
     console.log('myAP is now:', myAP);
     console.log('fungusHP is now', fungusHP);
@@ -46,23 +30,23 @@ function render() {
     $('#ap-meter').val(myAP);
     $('#hp-meter').val(fungusHP);
 
-
+    // still working on getting this interval/health gain section to work
     // if (fungusHP < 50 ) {
     //     $('#fungusHPScore').setInterval(growFungus, 1000);
     // };
 
-    if ( fungusHP === 0 && myAP > 0 ) {
-        $('#walk').toggleClass('dead');
-    } else if ( fungusHP > 0 && myAP === 0 ) {
-        $('#walk').toggleClass('jump');
-    };
+    if ( fungusHP === 0 && myAP > 0 ) { // start conditional to display fight results on DOM
+        $('#walk').toggleClass('dead'); // if mushroom loses, it dies
+    } else if ( fungusHP > 0 && myAP === 0 ) { // or if I lose
+        $('#walk').toggleClass('jump'); //mushroom happy
+    }; // end conditional 
+} // end render function
 
-    
-}
-
+// trying to link this function to setInterval line in 
+// render function, but can't get to work, so commenting out for now
 // function growFungus() {
 //     fungusHP++;
-// }
+// } // end growFungus function
 
 function scepterAttack() {
     // console.log('scepter!');
