af3e37103174f18be4a88443388e679cc4722ba3
Dave Nash <davidjnash2@gmail.com>
2023-05-08 22:35:12 -0500

    added span ids to scores in DOM, and linked to variables in render function to modify score text, tried changin css but did not function as desire so returned to original state, and got all attack functions to decrease scores correctly with state variables declared globally and render function called in each attack

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+
 let fungusHP = 100;
 let myAP = 100;
+
+
 
 function onReady() {
     console.log('jQuery ready!');
@@ -25,30 +28,62 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 }
+   
+function render() {
+
+    if ( myAP < 0 ) {
+        myAP = 0
+    };
+    
+    if ( fungusHP < 0 ) {
+        fungusHP = 0
+    };
+    
+    // if ( fungusHP === 0 && myAP > 0 ) {
+    //     $('#walk').css('.freaky-fungus.dead');
+    // } else if ( myAP === 0 && fungusHP > 0 ){
+    //     $('#walk').css('.freaky-fungus.jump');
+    //     $('#attack-btn').css('disabled');
+    // } else {
+    // };
+
+
+    console.log('myAP is now:', myAP);
+    console.log('fungusHP is now', fungusHP);
+    $('#myAPScore').text(myAP);
+    $('#fungusHPScore').text(fungusHP);
+
+}
 
 function scepterAttack() {
-    console.log('scepter!');
-    // myAP -= 12;
-    // fungusHP -= 14;
-}
+    // console.log('scepter!');
+    
+   
+    myAP -= 12;
+    fungusHP -= 14;
+    
+    render();
+    // give disabled attribute if myAP === 0
+     
+} // end scepterAttack function
 
 function entangleAttack() {
     console.log('entangle!');
     myAP -= 23;
     fungusHP -= 9;
-    
-
-}
+    render();
+} // end entangleAttack function
 
 function bladeAttack() {
     console.log('blade!');
     myAP -= 38;
-    fungusHP -= 47
-    
-}
+    fungusHP -= 47;
+    render();
+} // end bladeAttack function
 
 function fireAttack(){
     console.log('fire!');
     myAP -= 33;
     fungusHP -= 25;
-}
+    render();
+} // end fireAttack function
